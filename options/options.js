35ed0887c3bfc96d260d8990cdd16ae3a756d1e3
Crowdbotics <team@crowdbotics.com>
2023-06-29 06:16:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pbm-booking-care-41800",
+  url: "https://pbm-booking-care-41800.botics.co",
+  api: "https://pbm-booking-care-41800.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
